451316a04ab9242461596f635be3181bb4a9237e
Hartrickb <Hartrickb@users.noreply.github.com>
2017-01-16 12:46:54 -0500

    feat: changed to an actionSheet with different colors

--- AlertView/ViewController.swift
+++ AlertView/ViewController.swift
@@ -23,9 +23,14 @@ class ViewController: UIViewController {
 
     @IBAction func callAlert(_ sender: Any) {
         
-        let alert = PCLBlurEffectAlert.Controller(title: "Title", message: "Message", effect: UIBlurEffect(style: .light), style: .alert)
+        let alert = PCLBlurEffectAlert.Controller(title: "Title", message: "Message", effect: UIBlurEffect(style: .extraLight), style: .actionSheet)
         let alertButton = PCLBlurEffectAlert.AlertAction(title: "Cancel", style: .cancel, handler: nil)
         alert.addAction(alertButton)
+        
+        alert.configure(cornerRadius: 10)
+        alert.configure(overlayBackgroundColor: UIColor(red: 0, green: 0, blue: 100, alpha: 0.2))
+        alert.configure(titleFont: .systemFont(ofSize: 30), titleColor: .gray)
+        alert.configure(messageFont: .systemFont(ofSize: 20), messageColor: .black)
         
         alert.show()
         
